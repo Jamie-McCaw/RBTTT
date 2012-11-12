8fad12ea09680469da57339139e382d7052d87ea
Jamie-McCaw <psiral@yahoo.com>
2012-11-12 09:53:25 -0500

    Fixed formating

--- lib/tictactoe/ai.rb
+++ lib/tictactoe/ai.rb
@@ -1,66 +1,64 @@
-require 'rubygems'
-require 'pry'
 class AI
     
-    attr_accessor :type
+  attr_accessor :type
 
-    def initialize(type)
-      @type = type
-      @infinity = Float::INFINITY
-      @best_move = nil
-    end
+  def initialize(type)
+    @type = type
+    @infinity = Float::INFINITY
+    @best_move = nil
+  end
 
-    def make_move(board)
-      if board.cells.count == 16 && board.available_moves == 15
-        if board.cells[5] == 5
-          @best_move = 5
-        else 
-          @best_move = 6
-        end
-      elsif board.cells.count == 16 && board.available_moves == 13
-        if board.cells[6] == 6
-          @best_move = 6
-        else
-          @best_move = 10
-        end
-      else
-        negamax(board, @type, 1, -@infinity, @infinity)
+  def make_move(board)
+    if board.cells.count == 16 && board.available_moves == 15
+      if board.cells[5] == 5
+        @best_move = 5
+      else 
+        @best_move = 6
       end
-      return @best_move
-    end
-
-    def opponent(piece)
-      piece == 'X' ? 'O' : 'X'
-    end
-
-    def winner(board, player)
-      if board.winner == player
-        return 1
-      elsif board.winner == opponent(player)
-        return -1
+    elsif board.cells.count == 16 && board.available_moves == 13
+      if board.cells[6] == 6
+        @best_move = 6
       else
-        return 0
+        @best_move = 10
       end
+    else
+      negamax(board, @type, 1, -@infinity, @infinity)
     end
+    return @best_move
+  end
 
-    def negamax(board, player, depth, alpha, beta)
-      if board.game_over?
-        return winner(board, player)
-      else
-        best_rank = -@infinity
-        opponent = opponent(player)
+  def opponent(piece)
+    piece == 'X' ? 'O' : 'X'
+  end
 
-        board.available_spaces.each do |move|
-          current_board = board.dup
-          current_board.move(move, player)
-          rank = -negamax(current_board, opponent, depth - 1, -beta, -alpha)
-          if rank > alpha
-            alpha = rank
-            @best_move = move if depth == 1
-          end
-          break if alpha >= beta
+  def winner(board, player)
+    if board.winner == player
+      return 1
+    elsif board.winner == opponent(player)
+      return -1
+    else
+      return 0
+    end
+  end
+
+  def negamax(board, player, depth, alpha, beta)
+    if board.game_over?
+      return winner(board, player)
+    else
+      best_rank = -@infinity
+      opponent = opponent(player)
+
+      board.available_spaces.each do |move|
+        current_board = board.dup
+        current_board.move(move, player)
+        rank = -negamax(current_board, opponent, depth - 1, -beta, -alpha)
+        if rank > alpha
+          alpha = rank
+          @best_move = move if depth == 1
         end
-        return alpha
-      end 
-    end
+        break if alpha >= beta
+      end
+      return alpha
+    end 
+  end
 end
--- spec/tictactoe/ai_spec.rb
+++ spec/tictactoe/ai_spec.rb
@@ -2,35 +2,69 @@ require 'spec_helper'
 
 	describe AI do
 
-	let(:board) { Board.new }
 	let(:ai)	{ AI.new('O') }
 
-	  describe "ai" do
+    context "3x3" do
 
-	    it "makes the computer win" do
-	      board.move(0,'O')
-	      board.move(4,'X')
-	      board.move(3,'X')
-	      board.move(1,'O')
-	      ai.make_move(board).should == 2
-	    end
+	  let(:board) { Board.new }
 
-	    it "makes the computer block" do
-	      board.move(3, 'X')
-	      board.move(0, 'O')
-	      board.move(5, 'X')
-	      ai.make_move(board).should == 4
-	    end
-	  end
+      describe "ai" do
 
-    describe "player wins" do
-      it "shows positive for player winning" do
-        board.stub(:winner) { 'X' }
-        ai.winner(board, 'X').should == 1
+        it "makes the computer win" do
+          board.move(0,'O')
+          board.move(4,'X')
+          board.move(3,'X')
+          board.move(6,'X')
+          board.move(1,'O')
+          ai.make_move(board).should == 2
+        end
+
+        it "makes the computer block" do
+          board.move(3, 'X')
+          board.move(0, 'O')
+          board.move(5, 'X')
+          ai.make_move(board).should == 4
+        end
       end
-      it "shows negative for opponent winning" do
-        board.stub(:winner) { 'X' }
-        ai.winner(board, 'O').should == -1
+
+      describe "player wins" do
+        it "shows positive for player winning" do
+          board.stub(:winner) { 'X' }
+          ai.winner(board, 'X').should == 1
+        end
+        it "shows negative for opponent winning" do
+          board.stub(:winner) { 'X' }
+          ai.winner(board, 'O').should == -1
+        end
+      end
+    end
+
+    context "4x4" do
+
+    let(:board) { Board.new([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]) }
+
+      describe "ai" do
+        it "makes the computer win" do
+          board.move(0,'O')
+          board.move(1,'O')
+          board.move(2,'O')
+          board.move(4,'X')
+          board.move(5,'X')
+          board.move(14,'X')
+          board.move(15,'X')
+          ai.make_move(board).should == 3
+        end
+
+        it "makes the computer block" do
+          board.move(0,'O')
+          board.move(1,'O')
+          board.move(9,'O')
+          board.move(4,'X')
+          board.move(5,'X')
+          board.move(6,'X')
+          board.move(15,'X')
+          ai.make_move(board).should == 7
+        end
       end
     end
 	end
